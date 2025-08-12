df716ae697ad8cb674dec76de4b43699025d8874
Oliver Whittaker <oliver.whittaker@paramountdigital.co.uk>
2025-08-12 10:08:45 +0000

    Initial commit

---
+++ single-property.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * The template for displaying all single properties
+ *
+ * @package WordPress
+ * @subpackage Beluxe_Theme
+ * @since Beluxe 1.0
+ */
+
+get_header(); ?>
+
+<?php
+    // Get custom fields
+    $yearBuilt = get_field('year_built');
+    $bedrooms = get_field('bedrooms');
+    $bathrooms = get_field('bathrooms');
+    $buildSize = get_field('build_size');
+    $terraceSize = get_field('terrace_size');
+    $plotSize = get_field('plot_size');
+    $location_terms = get_the_terms(get_the_ID(), 'locations');
+    $first_location = $location_terms[0]->name;
+?>
+
+<section>
+
+    <div class="container">
+
+        <div class="property-details">
+            <div class="property-detail">
+                <img src="/wp-content/uploads/2025/08/calendar_OFF-1.png"><p class="detail">Year Built</p><hr class="detail-sep"/><p class="value"><?php echo esc_html($yearBuilt); ?></p>
+            </div>
+            <div class="property-detail">
+                <img src="/wp-content/uploads/2025/08/bed_OFF-8.png"><p class="detail">Bedrooms</p><hr class="detail-sep"/><p class="value"><?php echo esc_html($bedrooms); ?></p>
+            </div>
+            <div class="property-detail">
+                <img src="/wp-content/uploads/2025/08/BATHROOMS_OFF-8.png"><p class="detail">Bathrooms</p><hr class="detail-sep"/><p class="value"><?php echo esc_html($bathrooms); ?></p>
+            </div>
+            <div class="property-detail">
+                <img src="/wp-content/uploads/2025/08/HOUSE_OFF-8.png"><p class="detail">Built Size</p><hr class="detail-sep"/><p class="value"><?php echo esc_html($buildSize); ?>M²</p>
+            </div>
+            <div class="property-detail">
+                <img src="/wp-content/uploads/2025/08/TERRACE_OFF-1.png"><p class="detail">Terrace Size</p><hr class="detail-sep"/><p class="value"><?php echo esc_html($terraceSize); ?>M²</p>
+            </div>
+            <div class="property-detail">
+                <img src="/wp-content/uploads/2025/08/PLOT_OFF-8.png"><p class="detail">Plot Size</p><hr class="detail-sep"/><p class="value"><?php echo esc_html($plotSize); ?>M²</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class="col-6 property-left">
+
+            <h2 class="property-content-title">Luxury Villas in <?php echo esc_html($first_location); ?></h2>
+            <p class="property-content-p"><?php echo wp_strip_all_tags( get_the_content() ); ?></p>
+
+        </div>
+
+        <div class="col-6 property-right">
+
+            <h2 class="property-content-title pointers-title">Luxury Villas in <?php echo esc_html($first_location); ?></h2>
+
+            <?php if ( have_rows('key_features') ) : ?>
+                <ul class="key-features">
+                    <?php while ( have_rows('key_features') ) : the_row(); ?>
+                        <li><?php the_sub_field('feature'); ?></li>
+                    <?php endwhile; ?>
+                </ul>
+            <?php endif; ?>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <?php $gallery = get_field('property_gallery'); ?>
+        <?php if ($gallery && is_array($gallery)) : ?>
+        <div class="property-gallery-swiper swiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($gallery as $image) : ?>
+                    <div class="swiper-slide">
+                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" style="width:100%;height:auto;" />
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+        <?php endif; ?>
+
+    </div>
+
+</section>
+
+<?php get_footer(); ?>
+
+<script>
+    const propertyGallerySwiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+</script>
+
+<style>
+    .property-gallery-swiper {
+        padding-top: 100px;
+    }
+</style>
